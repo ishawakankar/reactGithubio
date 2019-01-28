e0783c8293839808007ac28b73ebd2df41858d6f
ishawakankar <iwakankar@hotmail.com>
2019-01-28 13:07:17 +0530

    Updates

---
+++ precache-manifest.1e16adfe57082cd1cea518af5b448db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactGithubio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "add20f0badff4d733447",
+    "url": "/reactGithubio/static/js/runtime~main.add20f0b.js"
+  },
+  {
+    "revision": "d4ad1a1d564ba0fe4e5b",
+    "url": "/reactGithubio/static/js/main.d4ad1a1d.chunk.js"
+  },
+  {
+    "revision": "ed13d4b1b1cc00ad2d0d",
+    "url": "/reactGithubio/static/js/1.ed13d4b1.chunk.js"
+  },
+  {
+    "revision": "d4ad1a1d564ba0fe4e5b",
+    "url": "/reactGithubio/static/css/main.a52efc42.chunk.css"
+  },
+  {
+    "revision": "cf0f5be34ebb3b183aca6167b72bca1a",
+    "url": "/reactGithubio/index.html"
+  }
+];
